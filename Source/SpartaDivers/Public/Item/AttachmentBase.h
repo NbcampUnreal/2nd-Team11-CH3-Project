1a85d395a429288e50e15b5d4551316f35a0f4a8
fucidin310 <fucidin310@gmail.com>
2025-02-19 20:58:39 +0900

    Impl Item
    Impl Item

---
+++ Source/SpartaDivers/Public/Item/AttachmentBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Item/ItemBase.h"
+#include "AttachmentBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPARTADIVERS_API UAttachmentBase : public UItemBase
+{
+	GENERATED_BODY()
+	
+};
