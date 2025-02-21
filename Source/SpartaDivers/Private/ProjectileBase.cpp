09242b98b56694f52a2ba5382db530bee4ea88b9
fucidin310 <fucidin310@gmail.com>
2025-02-21 20:50:28 +0900

    Impl_RagedMinion
    Impl_RagedMinion
    Impl_Projectile

---
+++ Source/SpartaDivers/Private/ProjectileBase.cpp
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ProjectileBase.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "Components/SphereComponent.h"
+
+// Sets default values
+AProjectileBase::AProjectileBase()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	SphereComponent = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComponent"));
+	SetRootComponent(SphereComponent);
+
+	MeshComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("MeshComponent"));
+	MeshComponent->SetupAttachment(SphereComponent);
+
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileMovementComponent"));
+	ProjectileMovementComponent->bRotationFollowsVelocity = true;
+}
+
+void AProjectileBase::InitProjectile(float InitVelocity)
+{
+	if (ProjectileMovementComponent)
+	{
+		ProjectileMovementComponent->Velocity = InitVelocity * GetActorForwardVector();
+	}
+}
---
+++ Source/SpartaDivers/Public/Enemy/RangedMinion.h
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SDEnemyBase.h"
+#include "RangedMinion.generated.h"
+
+class AEnemyProjectile;
+
+/**
+ * 
+ */
+UCLASS()
+class SPARTADIVERS_API ARangedMinion : public ASDEnemyBase
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void Attack() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	UAnimMontage* AttackMontage;
+
+	UPROPERTY(EditDefaultsOnly)
+	TSubclassOf<AEnemyProjectile> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	float VelocityOfProjectile = 100.f;
+
+	UPROPERTY(EditDefaultsOnly)
+	FName FireSocketName;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void Fire();
+};
