3f04948199976cbda4b7409db5b0d02fb3282aa8
dlsgjs100 <inkskku@gmail.com>
2025-02-21 23:23:30 +0900

    Add : SpawnVolume
    SpawnVolume을 구현했습니다.
    할당한 Mission Type에 따라서 Spawning BoX안의 랜점좌표에 등록한 적이 확률에 따라서 스폰합니다.

--- Source/SpartaDivers/Public/MissionDataRow.h
+++ Source/SpartaDivers/Public/MissionDataRow.h
@@ -12,7 +12,7 @@ enum class EMissionType : uint8
     Eliminate UMETA(DisplayName = "Eliminate"), // 적 전부 섬멸
     Survive UMETA(DisplayName = "Survive"),     // 일정 시간 생존
     Capture UMETA(DisplayName = "Capture"),     // 점령
-    Escape UMETA(DisplayName = "Escape")       // 탈출
+    BossCombat UMETA(DisplayName = "BossCombat")       // 보스 전투
 };
 
 USTRUCT(BlueprintType)
@@ -24,20 +24,16 @@ public:
     // Type of Mission
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     EMissionType MissionType;
-    // Chance for Spawning Enemies
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    TMap<TSubclassOf<AActor>, float> EnemySpawnChances;
     // Score
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     int32 ScoreReward;
+    // Enemy Count
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 EnemyCount;
     // Time Limit
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
     float SurviveTime;
     UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    float EscapeTimeLimit;
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
     float CaptureTime;
-    // Destinations for Escape
-    UPROPERTY(EditAnywhere, BlueprintReadWrite)
-    FVector TargetLocation;
+    
 };
